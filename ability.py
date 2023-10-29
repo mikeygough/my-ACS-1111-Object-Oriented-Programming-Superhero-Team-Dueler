9077bbd22f8d55e0f811b2d90f70202908855704
mikey <mikey@hey.com>
2023-10-29 13:38:03 -0500

    add ability class

---
+++ ability.py
@@ -0,0 +1,22 @@
+import random
+
+
+class Ability:
+    def __init__(self, name: str, max_damage: int):
+        """Instance properties:
+        name: string
+        max_damage: integer
+        """
+        self.name = name
+        self.max_damage = max_damage
+
+    def attack(self):
+        """ Return a value between 0 and the value set by self.max_damage. """
+        
+        return random.randint(0, self.max_damage)
+    
+    
+if __name__ == "__main__":
+    ability = Ability("Debugging Ability", 20)
+    print(ability.name)
+    print(ability.attack())
--- hero.py
+++ hero.py
@@ -1,21 +1,36 @@
 import random
 
+
 class Hero:
-    def __init__(self, name, starting_health=100):
-        ''' Instance properties:
-            name: string
-            starting_health: integer
-            current_health: integer
-        '''
-        
+    def __init__(self, name: str, starting_health: int = 100):
+        """Instance properties:
+        name: string
+        starting_health: integer
+        current_health: integer
+        """
+
         self.name = name
         self.starting_health = starting_health
         # at initialization, current health is equal to starting_health
         self.current_health = starting_health
-        
+
+    def add_ability(self, ability):
+        pass
+    
+    def attack(self):
+        pass
+    
+    def defend(self, incoming_damage: int):
+        pass
+    
+    def take_damage(self, damage: int):
+        pass
+    
+    def is_alive(self):
+        pass
+    
     def fight(self, opponent):
-        ''' Current Hero will take turns fighting the opponent hero passed in.
-        '''
+        """Current Hero will take turns fighting the opponent hero passed in."""
         # total health
         total_p = self.starting_health + opponent.starting_health
         # self probability
@@ -24,7 +39,7 @@ class Hero:
         opponent_p = opponent.starting_health / total_p
         # outcome
         outcome = random.uniform(0, 1)
-        
+
         if outcome <= self_p:
             # self wins
             print(f"{self.name} defeats {opponent.name}!")
@@ -32,7 +47,15 @@ class Hero:
             # opponent wins
             print(f"{opponent.name} defeats {self.name}!")
 
-        
+
+class Armor:
+    def __init__(self, name: str, max_block: int):
+        pass
+
+    def block(self):
+        pass
+
+
 # prevent the code from being run when the script is imported by another script
 if __name__ == "__main__":
     naruto = Hero("Naruto Uzumaki", 100)
