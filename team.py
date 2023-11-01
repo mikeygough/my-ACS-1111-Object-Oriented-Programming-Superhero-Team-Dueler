1b8beb39a16e8dd8dd89a78588ee1430b5fd2836
mikey <mikey@hey.com>
2023-11-01 17:26:56 -0500

    implemented Team class

---
+++ team.py
@@ -0,0 +1,24 @@
+class Team():
+    def __init__(self, name):
+        """ Initialize your team with its team name and an empty list of heroes """
+        self.name = name
+        self.heroes = list()
+    
+    def add_hero(self, hero):
+        """ Add Hero object to self.heroes. """
+        self.heroes.append(hero)
+        
+    def remove_hero(self, name):
+        """ Remove hero from heroes list.
+        If Hero isn't found return 0."""
+        foundHero = False
+        for hero in self.heroes:
+            if hero.name == name:
+                self.heroes.remove(hero)
+                foundHero = True
+        if not foundHero:
+            return 0
+    
+    def view_all_heroes(self):
+        """ Prints out all heroes to the console. """
+        [print(hero.name) for hero in self.heroes]
